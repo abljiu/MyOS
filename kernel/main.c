31ea09340ace758b3b1ab406fe677c1791b08399
abljiu <1160433376@qq.com>
2024-09-12 20:26:14 +0800

    8E

--- kernel/main.c
+++ kernel/main.c
@@ -5,6 +5,12 @@ int main(void)
 {
    put_str("I am kernel\n");
    init_all();
+
+   void *addr = get_kernel_pages(3);
+   put_str("\n get_kernel_page start vaddr is ");
+   put_int((uint32_t)addr);
+   put_str("\n");
+
    while (1)
       ;
    return 0;
