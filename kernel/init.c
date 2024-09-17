28d852c30dbd52acfa48d8905dbfeaf5185320fb
abljiu <1160433376@qq.com>
2024-09-17 22:54:58 +0800

    10B

--- kernel/init.c
+++ kernel/init.c
@@ -4,6 +4,7 @@
 #include "../device/timer.h"
 #include "../kernel/memory.h"
 #include "../thread/thread.h"
+#include "../device/console.h"
 
 /*负责初始化所有模块 */
 void init_all()
@@ -13,4 +14,5 @@ void init_all()
    timer_init();  // 初始化 PIT
    mem_init();    // 初始化内存管理系统
    thread_init(); // 初始化线程环境
+   console_init(); //控制台初始化最好放在开中断之前
 }
