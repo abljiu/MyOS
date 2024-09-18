ca5b72ddb081160c74ac446be405889903bf42fc
abljiu <1160433376@qq.com>
2024-09-18 11:36:31 +0800

    10D

--- kernel/init.c
+++ kernel/init.c
@@ -5,14 +5,16 @@
 #include "../kernel/memory.h"
 #include "../thread/thread.h"
 #include "../device/console.h"
+#include "../device/keyboard.h"
 
 /*负责初始化所有模块 */
 void init_all()
 {
    put_str("init_all\n");
-   idt_init();    // 初始化中断
-   timer_init();  // 初始化 PIT
-   mem_init();    // 初始化内存管理系统
-   thread_init(); // 初始化线程环境
-   console_init(); //控制台初始化最好放在开中断之前
+   idt_init();      // 初始化中断
+   timer_init();    // 初始化 PIT
+   mem_init();      // 初始化内存管理系统
+   thread_init();   // 初始化线程环境
+   console_init();  // 控制台初始化最好放在开中断之前
+   keyboard_init(); // 初始化键盘
 }
