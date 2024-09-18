b18c931303584dcd2d1a73cfe87fef43ef2059b6
abljiu <1160433376@qq.com>
2024-09-18 20:18:12 +0800

    10E

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -52,8 +52,8 @@ static void pic_init(void)
    outb(PIC_S_DATA, 0x01); // ICW4: 8086模式, 正常EOI
 
    /*测试键盘，只打开键盘中断，其他全部关闭*/
-   outb(PIC_M_DATA, 0xfd);
-   outb(PIC_S_DATA, 0xff);
+   outb (PIC_M_DATA, 0xfc);
+   outb (PIC_S_DATA, 0xff); 
 
    put_str("   pic_init done\n");
 }
